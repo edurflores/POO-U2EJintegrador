b0d4a9437c4e6270ee3feb0e802c2422aa70fcb3
Eduardo Flores <81997573+edurflores@users.noreply.github.com>
2021-05-11 02:43:21 -0300

    Update claseProyecto.py

--- claseProyecto.py
+++ claseProyecto.py
@@ -12,7 +12,22 @@ class Proyecto:
             self.__palabrasClave = palcla # Ojo, podria ser una lista de palabras
     def GetIdProy(self):
         return self.__idProyecto
+    def GetTitulo(self):
+        return self.__titulo
+    def GetPalabras(self):
+        return self.__palabrasClave
     def SetPuntos(self,punt):
         if type(punt) is int:
             self.__puntaje = punt
-    
+    def GetPuntos(self):
+        return self.__puntaje
+    def __gt__(self,otroproy):
+        if self.__puntaje > otroproy.GetPuntos():
+            return True
+        else:
+            return False
+    def MostrarProyecto(self):
+            print('ID de proyecto:{}'.format(str(self.__idProyecto)))
+            print('Titulo:{}'.format(str(self.__titulo)))
+            print('Palabras clave:{}'.format(str(self.__palabrasClave)))
+            print('Puntaje obtenido:{}'.format(str(self.__puntaje)))
